25d5373ca12978f0c8963d47536f8c81e9282a84
bbarn3y <babuafi@inf.elte.hu>
2022-10-28 06:33:04 +0200

    7. gyakorlat starter kit

--- gyak6/gyak6.js
+++ gyak6/gyak6.js
@@ -12,10 +12,10 @@ ctxP.strokeText('Text', 20, 50);
 ctxP.fillRect(100, 20, 40, 20);
 ctxP.strokeRect(100, 60, 40, 20);
 
-const tramImgEl = document.getElementById('trapImg');
+const wikiImgEl = document.getElementById('wikiImg');
 const image = new Image();
 image.onload = function() {
-    ctxP.drawImage(tramImgEl, 150, 0);
+    ctxP.drawImage(wikiImgEl, 150, 0);
 }
 image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png'
 
