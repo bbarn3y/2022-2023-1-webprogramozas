4276403f5b1683cd817fb31b265f3c4f8eb84805
bbarn3y <babuafi@inf.elte.hu>
2022-11-18 13:49:16 +0100

    10. gyakorlat

--- gyak10/file.php
+++ gyak10/file.php
@@ -3,9 +3,7 @@
 $personsFile = fopen("persons.json", "r");
 
 $personsAsString = fread($personsFile, filesize("persons.json"));
-echo $personsAsString;
 $parsedPersons = json_decode($personsAsString, true);
-print_r($parsedPersons);
 
 ?>
 
@@ -13,7 +11,77 @@ print_r($parsedPersons);
 <?php
     foreach ($parsedPersons as $person) : ?>
     <li>
-        <?= $person['name'] ?>
+        <?= $person['name'] ?>,
+        <?= $person['age'] ?> years old,
+        <?php
+            switch($person['sex']) {
+                case 'M':
+                    echo 'Male';
+                    break;
+                case 'F':
+                    echo 'Female';
+                    break;
+                default:
+                    echo 'Other';
+                    break;
+            }
+        ?>
     </li>
 <?php endforeach; ?>
 </ul>
+
+<?php
+
+function prettyPrintSex(string $sex): string {
+    switch($sex) {
+        case 'M':
+            return 'Male';
+        case 'F':
+            return 'Female';
+        default:
+            return 'Other';
+    }
+}
+
+$personsCSVFile = fopen('persons.csv', 'w');
+
+fwrite($personsCSVFile, 'name,age,sex' . PHP_EOL);
+foreach ($parsedPersons as $person) {
+    fwrite($personsCSVFile, $person['name'] . ',' . $person['age'] . ',' . prettyPrintSex($person['sex']) . PHP_EOL);
+}
+
+fclose($personsCSVFile);
+
+?>
+
+<?php
+
+$personsFromCSV = [];
+
+$personsCSVFile = fopen('persons.csv', 'r');
+
+$headers = fgetcsv($personsCSVFile, NULL, ',');
+while($row = fgetcsv($personsCSVFile, NULL, ',')) {
+    // print_r($row);
+    $personsFromCSV[] = array_combine($headers, $row);
+}
+
+fclose($personsCSVFile);
+
+?>
+
+<ul>
+    <?php foreach ($personsFromCSV as $person) : ?>
+    <li>
+        <?= $person['name'] ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
+
+<?php
+    $personsFromJSONOneLiner = json_decode(file_get_contents('persons.json'), true);
+
+    print_r($personsFromJSONOneLiner);
+
+    file_put_contents('tmp.txt', "asdasd asdd asd asd  asd\n sadasd");
+?>
--- gyak10/interface.php
+++ gyak10/interface.php
@@ -1,16 +1,107 @@
 <?php
 
-function queryMySQL() {
-    // Create connection
-    $conn = new mysqli('db4free.net:3306', 'bbarney', 'pBCFjWbX!2.t8DD', 'persons');
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT name, age, sex FROM person";
-    $result = $conn->query($sql);
-    return result;
+interface PersonsApi {
+    function getPersons(): array;
+    function getPersonsOverAge(int $age): array;
 }
 
+abstract class PersonsFromWhatever implements PersonsApi {
+    abstract function getPersons(): array;
+
+    function getPersonsOverAge(int $age): array {
+        $result = [];
+        foreach ($this->getPersons() as $person) {
+            if ($person['age'] >= $age) {
+                $result[] = $person;
+            }
+        }
+        return $result;
+    }
+}
+
+class PersonsFromJSON extends PersonsFromWhatever {
+    function getPersons(): array {
+        return json_decode(file_get_contents('persons.json'), true);
+    }
+}
+
+class PersonsFromMySQL extends PersonsFromWhatever {
+    function getPersons(): array {
+        // Create connection
+        $conn = new mysqli('db4free.net:3306', 'bbarney', 'pBCFjWbX!2.t8DD', 'persons');
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT name, age, sex FROM person";
+        $result = $conn->query($sql);
+
+        $persons = [];
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $persons[] = $row;
+            }
+        } else {
+            echo "0 results";
+        }
+        $conn->close();
+
+        return $persons;
+    }
+}
+
+//function queryMySQL() {
+//    // Create connection
+//    $conn = new mysqli('db4free.net:3306', 'bbarney', 'pBCFjWbX!2.t8DD', 'persons');
+//    // Check connection
+//    if ($conn->connect_error) {
+//        die("Connection failed: " . $conn->connect_error);
+//    }
+//
+//    $sql = "SELECT name, age, sex FROM person";
+//    $result = $conn->query($sql);
+//    return result;
+//}
+
+?>
+
+<?php
+
+$personsAPI = new PersonsFromMySQL();
+
+?>
+
+<h1>All persons</h1>
+<ul>
+    <?php foreach ($personsAPI->getPersons() as $person) : ?>
+    <li>
+        <?= $person['name'] ?>
+    </li>
+    <?php endforeach; ?>
+</ul>
+
+<?php
+    $ageFilter = $_GET['age'] ?? NULL;
+?>
+
+<h1>Filtered persons</h1>
+<form action="">
+    <input type="number" name="age" />
+    <button type="submit">Submit</button>
+</form>
+
+<?php
+if (is_numeric($ageFilter)):
+?>
+    <ul>
+        <?php foreach ($personsAPI->getPersonsOverAge($ageFilter) as $person) : ?>
+            <li>
+                <?= $person['name'] ?>,
+                <?= $person['age'] ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php
+endif;
 ?>
