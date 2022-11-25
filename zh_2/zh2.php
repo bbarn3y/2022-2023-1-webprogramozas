f8d9d9470be0e09464b030eb792ba012196683ac
bbarn3y <babuafi@inf.elte.hu>
2022-11-25 08:16:27 +0100

    ZH2

---
+++ zh_2/zh2.php
@@ -0,0 +1,70 @@
+<?php
+# 1.1
+
+
+# 1.2
+
+
+# 1.3
+
+
+?>
+
+<table>
+    <thead>
+        <tr>
+            <td>Title</td>
+            <th>Author</th>
+            <th>Release</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        # 1.4
+
+        ?>
+    </tbody>
+</table>
+
+<?php
+# 1.5
+
+?>
+
+<ul>
+    <?php
+
+    ?>
+</ul>
+
+<!--
+# 1.6
+-->
+<form>
+
+</form>
+
+<?php
+
+?>
+
+
+<?php
+# 1.7
+
+?>
+
+
+<?php
+# 1.8
+
+?>
+
+
+<?php
+# 1.9
+function medianReleaseDate($books) {
+
+}
+?>
+Median release date:
